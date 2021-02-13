80ec50f35bfe4eaa674baa77a61005c346dffbc3
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-13 16:33:17 -0300

    custom authMiddleware jwt apply

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js';
 import asyncHandler from 'express-async-handler'
-
+import { generateToken } from '../utils/generateToken.js';
 
 // @desc Auth user & get Token
 // @route POST /api/users/login
@@ -9,15 +9,57 @@ export const authUser=asyncHandler(async(req,res)=>{
     const {email,password}=req.body
     const user=await User.findOne({email})
     if(user && (await user.matchPassword(password))){
-        res.json({
+        return res.json({
             _id:user._id,
             name:user.name,
             email:user.email,
             isAdmin:user.isAdmin,
-            token:null,
+            token:generateToken(user._id),
         })
     }else{
         return res.status(401).json({message:'Invalid email or password'})
     }
 })
+// @desc Get user profile
+// @route POST /api/users/profile
+// @access Private
+export const getUserProfile=asyncHandler(async(req,res)=>{
+    const user=await User.findById(req.user._id)
+    if(user){
+        return res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+        })
+    }else{
+        return res.status(404).send({message:'User not found'})
+    }
+})
 
+// @desc Register new user
+// @route POST /api/users/
+// @access public 
+export const registerUser=asyncHandler(async(req,res)=>{
+    const {name,email,password}=req.body
+    const userExist=await User.findOne({email})
+   if(userExist){
+       return res.status(400).send({message:'User already exists'})
+   }
+   const user = await User.create({
+       name,
+       email,
+       password
+   })
+   if(user){
+    return res.status(201).json({
+        _id:user._id,
+        name:user.name,
+        email:user.email,
+        isAdmin:user.isAdmin,
+        token:generateToken(user._id),
+    })
+   }else{
+    return res.status(400).send({message:'Invalid user data , try again'})
+   }
+})
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebToken'
+import User from '../models/User.js';
+
+export const protect=async(req,res,next)=>{
+    let token
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            token=req.headers.authorization.split(' ')[1];
+            const decoded=jwt.verify(token,process.env.JWT_SECRET);
+            // console.log(decoded);
+            req.user=await User.findById(decoded.id).select('-password')
+            return next();
+        } catch (error) {
+            console.log(error);
+            return res.status(401).send({message:'No Authorized'})
+
+        }
+    }
+    if(!token){
+        // res.status(401).json({message:'Not Authorized,'})
+        return res.status(401).send({message:'No Authorized'})
+    }
+    next();
+} 
