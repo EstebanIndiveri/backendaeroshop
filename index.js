22b263349d509316b193bd5a2b7308a9c7652faa
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-12 21:27:28 -0300

    custom Error

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from 'express';
-import products from './db/products.js';
+// import products from './db/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errroMiddleware.js';
 
 dotenv.config();
 connectDB();
 const app=express();
 
-app.get('/api/products',(req,res)=>{
-    res.json(products);
-})
+app.use('/api/products',productRoutes);
 
-app.get('/api/products/:id',(req,res)=>{
-    const product=products.find(p=>p._id===req.params.id)
-    res.json(product);
-})
+app.use(notFound)
+
+app.use(errorHandler)
+
 
 const PORT=process.env.PORT || 5000
 
