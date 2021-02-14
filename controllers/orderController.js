80d3dcdddfd7d821acad2a2c7ab756d1d0e71623
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-14 18:12:36 -0300

    update paymentMethods

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -37,3 +37,23 @@ export const getOrderById=asyncHandler(async(req,res)=>{
        return res.status(404).send({message:'Order not found'})
    }
 })
+// @desc Update order to paid
+// @route Get /api/orders/:ID/poy
+// @access private 
+export const updateOrderToPaid=asyncHandler(async(req,res)=>{
+    const order=await Order.findById(req.params.id);
+    if(order){
+        order.isPaid=true;
+        order.paidAt=Date.now();
+        order.paymentResult={
+            id:req.body.id,
+            status:req.body.status,
+            update_time:req.body.update_time,
+            email_address:req.body.payer.email_address
+        }
+        const updatedOrder=await order.save();
+        return res.json(updatedOrder);
+    }else{
+        return res.status(404).send({message:'Order not found'})
+    }
+ })
