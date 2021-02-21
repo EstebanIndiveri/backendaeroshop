63298610981ce23a26aa68357047ec0d6598ac2e
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-21 13:58:20 -0300

    add multer and routes upload files images

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 // import products from './db/products.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -6,6 +7,8 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+
 import { errorHandler, notFound } from './middleware/errroMiddleware.js';
 import mercadopago from 'mercadopago';
 
@@ -149,7 +152,11 @@ app.post('/create-checkout-session', async (req, res) => {
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
 app.use('/api/orders',orderRoutes);
+app.use('/api/upload',uploadRoutes);
 app.get('/api/config/paypal',(req,res,next)=>res.send(process.env.PAYPAL_CLIENT_ID))
+const __dirname=path.resolve();
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+
 app.use(notFound)
 
 app.use(errorHandler);
