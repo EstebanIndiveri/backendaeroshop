9c867d610ae8a723c302cfbd5563ee3db89bd9ea
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-02-22 16:45:16 -0300

    rating and review added controller and route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-
+import morgan from 'morgan';
 import { errorHandler, notFound } from './middleware/errroMiddleware.js';
 import mercadopago from 'mercadopago';
 
@@ -26,6 +26,11 @@ import Order from './models/Order.js';
 dotenv.config();
 connectDB();
 const app=express();
+
+if(process.env.NODE_ENV==='development'){
+  app.use(morgan('dev'))
+}
+
 app.use(express.json())
 app.use(cors({origin:function(origin,callback){
   callback(null, origin);
